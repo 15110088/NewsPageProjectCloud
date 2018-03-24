b55f64e7b8619b201aa76e169a7b74e2a136b65c
lnghia18l <lnghia18l@gmail.com>
2018-03-24 20:55:46 +0700

    first commit

--- TinTuc/src/main/java/connect/connect.java
+++ TinTuc/src/main/java/connect/connect.java
@@ -5,7 +5,8 @@ import java.sql.DriverManager;
 
 public class connect {
 	 private static final String DRIVER_JDBC = "com.mysql.jdbc.Driver";
-	    private static final String URL_DB = "jdbc:mysql://35.230.89.36/demo";
+	    private static final String URL_DB = "jdbc:mysql://google/demo?cloudSqlInstance=test2-197607:us-west1:demo&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=root&password=";
+
 	    //private static final String URL_DB = "jdbc:mysql://localhost/demo";
 	    // Pass and User
 	    private static final String USER = "root";
