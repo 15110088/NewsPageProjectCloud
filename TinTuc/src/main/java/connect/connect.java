00ef248717dea13636bfc86a9abc70fde81eff21
lnghia18l <lnghia18l@gmail.com>
2018-03-25 02:19:03 +0700

    aa

--- TinTuc/src/main/java/connect/connect.java
+++ TinTuc/src/main/java/connect/connect.java
@@ -5,8 +5,9 @@ import java.sql.DriverManager;
 
 public class connect {
 	// private static final String DRIVER_JDBC = "com.mysql.jdbc.GoogleDriver";
-	 private static final String DRIVER_JDBC = "com.mysql.jdbc.Driver";
-	   private static final String URL_DB = "jdbc:google:mysql://test2-197607:us-west1:demo/demo?user=root&password=123";
+	// private static final String DRIVER_JDBC = "com.mysql.jdbc.Driver";
+	private static final String DRIVER_JDBC = "com.mysql.cj.jdbc.Driver";
+	private static final String URL_DB = "jdbc:google:mysql://test2-197607:us-west1:demo/demo?user=root&password=123";
 	  //private static final String URL_DB = "jdbc:mysql://localhost/demo";
 	    // Pass and User
 	    private static final String USER = "root";
