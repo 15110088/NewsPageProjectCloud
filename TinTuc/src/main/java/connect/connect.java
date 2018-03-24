2811a9b1fc95495d5a8104ee3d4390bc8356eeee
lnghia18l <lnghia18l@gmail.com>
2018-03-25 01:06:11 +0700

    first commit

--- TinTuc/src/main/java/connect/connect.java
+++ TinTuc/src/main/java/connect/connect.java
@@ -4,19 +4,19 @@ import java.sql.DriverManager;
 
 
 public class connect {
-	 private static final String DRIVER_JDBC = "com.mysql.jdbc.Driver";
-	    private static final String URL_DB = "jdbc:google:mysql://test2-197607:us-west1:demo/demo?user=root";
-	    //private static final String URL_DB = "jdbc:mysql://localhost/demo";
+	 private static final String DRIVER_JDBC = "com.mysql.jdbc.GoogleDriver";
+	   private static final String URL_DB = "jdbc:google:mysql://test2-197607:us-west1:demo/demo?user=root&password=";
+	   // private static final String URL_DB = "jdbc:mysql://localhost/demo";
 	    // Pass and User
-	   // private static final String USER = "root";
-	   // private static final String PASS = "";
+	    private static final String USER = "root";
+	    private static final String PASS = "";
 
 	    private static Connection conn;
 
 	    public static Connection getConnection() {
 	        try {
 	            Class.forName(DRIVER_JDBC);
-	            conn = DriverManager.getConnection("spring.datasource.url");
+	            conn = DriverManager.getConnection(URL_DB);
 	        } catch (Exception e) {
 	            System.out.println("Error connection " + e);
 	        }
